8738dc8dca3d65132a870f4d06ff407cc735b6e0
Asel <asel.alimbaeva.kk@gmail.com>
2022-08-22 06:12:52 +0300

    feat: realiz delete Difficult words

--- src/scripts/book/chechActiv.ts
+++ src/scripts/book/chechActiv.ts
@@ -12,21 +12,19 @@ class ChechActiv {
     if (!card?.getAttribute("id")) return;
     const cardID = card?.getAttribute("id");
     const cardStudiBtn = eventEl.target as HTMLElement;
-    if (cardStudiBtn) {
-      cardStudiBtn.closest(".item-page__studi") as HTMLElement;
-    }
-
     const cardDifficult = eventEl.target as HTMLElement;
-    if (cardDifficult) {
-      cardDifficult.closest(".item-page__difficult") as HTMLElement;
-    }
+    const cardDifficultDell = eventEl.target as HTMLElement;
 
-    if (!cardStudiBtn && !cardDifficult) {
+    if (!cardStudiBtn && !cardDifficult && !cardDifficultDell) {
       return;
     } else if (cardStudiBtn.classList.contains("item-page__studi")) {
       chechActivStudi.check(card, cardStudiBtn, cardID);
     } else if (cardDifficult.classList.contains("item-page__difficult")) {
       chechActivDifficult.add(cardDifficult, cardID);
+    } else if (
+      cardDifficultDell.classList.contains("item-page__difficult-delete")
+    ) {
+      chechActivDifficult.dell(cardDifficultDell, cardID);
     }
   }
 }
--- src/scripts/book/chechActivDifficult.ts
+++ src/scripts/book/chechActivDifficult.ts
@@ -2,17 +2,29 @@ const cardDifficults: string[] = [];
 
 class ChechActivDifficult {
   add(cardDifficult: HTMLElement, cardID: string | null) {
-    const cardq = cardDifficult.closest(
-      ".item-page__authorized"
-    ) as HTMLElement;
-    // console.log(card, cardq, cardID);
-    const cardDifficultDel = cardq.childNodes[5] as HTMLElement;
-    // console.log(cardDifficultDel);
+    const card = cardDifficult.closest(".item-page__authorized") as HTMLElement;
+    const cardDifficultDel = card.childNodes[5] as HTMLElement;
     cardDifficultDel.style.display = "block";
     cardDifficult.style.display = "none";
     if (!cardID) return;
     cardDifficults.push(cardID);
     localStorage.setItem("cardDifficults", `${cardDifficults}`);
+  }
+  dell(cardDifficultDell: HTMLElement, cardID: string | null) {
+    const card = cardDifficultDell.closest(
+      ".item-page__authorized"
+    ) as HTMLElement;
+    console.log(cardDifficultDell, card, cardID);
+    const cardDifficult = card.childNodes[3] as HTMLElement;
+    console.log(cardDifficult);
+    cardDifficult.style.display = "block";
+    cardDifficultDell.style.display = "none";
+    if (!cardID) return;
+    if (cardDifficults.indexOf(cardID) !== -1) {
+      const idCard = cardDifficults.indexOf(cardID);
+      cardDifficults.splice(idCard, 1);
+      localStorage.setItem(`cardDifficults`, `${cardDifficults}`);
+    }
   }
 }
 
