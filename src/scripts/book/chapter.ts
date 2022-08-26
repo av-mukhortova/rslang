c7684ac6c1df100e1f0e02835c59eb4b206e4bfb
Asel <asel.alimbaeva.kk@gmail.com>
2022-08-26 03:25:46 +0300

    feat: add change style for chapter

--- src/scripts/book/chapter.ts
+++ src/scripts/book/chapter.ts
@@ -1,7 +1,22 @@
 import Pages from "./pages";
 import "../../assets/styles/bookStyle/chapter.css";
 
+interface Key {
+  key: string[];
+}
 class Chapter {
+  studiKeys: string | null;
+  dificaltKeys: string | null;
+  countStudi: number;
+  countDifficalt: number;
+
+  constructor() {
+    this.studiKeys = localStorage.getItem("studi");
+    this.dificaltKeys = localStorage.getItem("cardDifficults");
+    this.countStudi = 0;
+    this.countDifficalt = 0;
+  }
+
   public create() {
     const body = document.querySelector("body");
     const chap = document.querySelector(".chapters") as HTMLElement;
@@ -11,10 +26,30 @@ class Chapter {
     }
     const chapters = document.createElement("section");
     chapters.setAttribute("class", "chapters");
-    for (let i = 0; i < 6; i += 1) {
+    for (let i = 0; i < 7; i += 1) {
+      this.countStudi = 0;
+      this.countDifficalt = 0;
+      this.count(i);
+      console.log(this.countStudi, this.countDifficalt, i);
       const chapter = document.createElement("div");
       chapter.setAttribute("id", `chapter-${i}`);
-      chapter.setAttribute("class", `chapter`);
+
+      if (this.countStudi === 30 && this.countDifficalt === 30) {
+        chapter.setAttribute("class", `chapter `);
+        chapter.style.backgroundColor = "green";
+        chapter.style.border = "2px solid red";
+      } else if (this.countStudi === 30) {
+        chapter.setAttribute("class", `chapter`);
+        chapter.style.backgroundColor = "green";
+        chapter.style.border = "";
+      } else if (this.countDifficalt === 30) {
+        chapter.setAttribute("class", `chapter`);
+        chapter.style.backgroundColor = "";
+        chapter.style.border = "2px solid red";
+      } else {
+        chapter.setAttribute("class", `chapter`);
+      }
+
       const number = document.createElement("p");
       number.textContent = `chapter ${i + 1}`;
       chapter.append(number);
@@ -29,6 +64,34 @@ class Chapter {
       if (!group) return;
       pages.getWordData(chapters, group);
     });
+  }
+
+  count(shapterNum: number) {
+    if (this.studiKeys) {
+      const studiKeys = JSON.parse(this.studiKeys);
+      if (studiKeys[shapterNum]) {
+        if (studiKeys[shapterNum].length === 30) {
+          studiKeys[shapterNum].forEach((el: Key) => {
+            if (el) {
+              this.countStudi += el.key.length === 20 ? 1 : 0;
+            }
+          });
+        }
+      }
+    }
+
+    if (this.dificaltKeys) {
+      const dificaltKeys = JSON.parse(this.dificaltKeys);
+      if (dificaltKeys[shapterNum]) {
+        if (dificaltKeys[shapterNum].length === 30) {
+          dificaltKeys[shapterNum].forEach((el: Key) => {
+            if (el) {
+              this.countDifficalt += el.key.length === 20 ? 1 : 0;
+            }
+          });
+        }
+      }
+    }
   }
 }
 
