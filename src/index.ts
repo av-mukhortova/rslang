b77fbed083217bfb1c34c8d9dfd6f5aa4eadeaa4
Asel <asel.alimbaeva.kk@gmail.com>
2022-08-28 14:18:47 +0300

    feat: add statistrcks

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import "./assets/styles/mainPage/mainPage.css";
 import "./assets/styles/style.css";
 import "./assets/styles/sprint_style.css";
-import "./assets/styles/style.scss";
 import App from "./scripts/app";
 
 const app = new App();
--- src/scripts/mainPage/main.ts
+++ src/scripts/mainPage/main.ts
@@ -2,6 +2,7 @@ import Header from "./header";
 import Menu from "./menu";
 import Authorization from "./authorization/authorization";
 import StudiBlockDiv from "./studiBlockDiv";
+import StatisticsText from "./statisticsText";
 
 class Main {
   start() {
@@ -9,19 +10,24 @@ class Main {
     const menu = new Menu();
     const authorization = new Authorization();
     const studiBlockDiv = new StudiBlockDiv();
+    const statisticsText = new StatisticsText();
 
     const body = document.querySelector("body") as HTMLElement;
     const herro = document.createElement("section") as HTMLElement;
     const studiBlock = document.createElement("section") as HTMLElement;
+    const statisticsBlock = document.createElement("section") as HTMLElement;
 
     herro.setAttribute("class", "herro");
     studiBlock.setAttribute("class", "main__studi-block");
+    statisticsBlock.setAttribute("class", "main__statistics-block");
 
     studiBlock.append(studiBlockDiv.create());
+    statisticsBlock.append(statisticsText.create());
 
     body.prepend(header.start());
     body.append(herro);
     body.append(studiBlock);
+    body.append(statisticsBlock);
 
     const menuBurger = document.querySelector(".header__burger");
     menuBurger?.addEventListener("click", () => {
