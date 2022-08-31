a5f30d8de8c8ecec01abf01cc37b0ee802d1f610
Asel <asel.alimbaeva.kk@gmail.com>
2022-08-31 09:21:37 +0300

    refactor: change and add class for menu and chapter files

--- src/scripts/book/chapter.ts
+++ src/scripts/book/chapter.ts
@@ -18,9 +18,9 @@ class Chapter {
   }
 
   public create() {
-    const book: HTMLDivElement | null = document.querySelector(".book");
+    const book: HTMLDivElement | null = document.querySelector(".bookPage");
     book?.classList.remove("hidden");
-    const main: HTMLDivElement | null = document.querySelector(".main");
+    const main: HTMLDivElement | null = document.querySelector(".mainPage");
     main?.classList.add("hidden");
 
     const chap = document.querySelector(".chapters") as HTMLElement;
@@ -65,7 +65,7 @@ class Chapter {
       const idChapter = (e.target as HTMLElement).closest("div") as HTMLElement;
       if (!idChapter?.getAttribute("id")) return;
       const group = idChapter?.getAttribute("id")?.split("-")[1];
-      book?.setAttribute("class", `chapter-${group}`);
+      book?.setAttribute("class", `bookPage chapter-${group}`);
       const pages = new Pages();
       if (!group) return;
       pages.getWordData(chapters, group);
